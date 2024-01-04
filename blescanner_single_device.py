25e164c2987ab78e890bcf10ed769598839cdfd9
Robert W.B. Linn <rwblinn@outlook.de>
2024-01-04 12:31:33 +0100

    Update blescanner_single_device.py

--- blescanner_single_device.py
+++ blescanner_single_device.py
@@ -4,7 +4,7 @@
 # File: blescanner_single_device.py
 # Date: 20240104
 # Author: Robert W.B. Linn
-# Scan for a single BLE device and parse the data published.
+# Example: Scan for a single BLE device and parse the data published.
 # Dependencies: blescanner.py - Tool to scan for Bluetooth Low Energy (BLE) devices.
 
 import subprocess
